6f8b3817cfe554417a0a3ae0fe758199b9f130a0
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2016-09-13 08:10:51 +0100

    Made camera follow player

--- src/main/scala/ui/app/Output.scala
+++ src/main/scala/ui/app/Output.scala
@@ -1,11 +1,10 @@
 package ui.app
 
-import rogueLike.movement.Position
 import core.GameState
+import rogueLike.movement.Position
 import rogueLike.output.Sprite
 
 import scalafx.scene.canvas.Canvas
-import scalafx.scene.paint.Color
 
 /**
   * Created by rob on 13/04/16.
@@ -15,17 +14,27 @@ object Output {
     val g2d = canvas.getGraphicsContext2D
     val size = 32
 
-    val height = 512
-    val width = 512
+    val (height, width) = (canvas.getHeight, canvas.getWidth)
+
+    val tiles = height / size
+
+    val offset = tiles / 2 - 1
 
     g2d.clearRect(0, 0, height, width)
 
-    state.entities.collect{
-      case sprite:Sprite =>
+    val (playerX, playerY) = state
+      .entities
+      .collectFirst { case e: Position if e.id == Main.playerID => e }
+      .map(pos => (pos.x, pos.y))
+      .get
+
+    state.entities.collect {
+      case sprite: Sprite =>
         g2d.setFill(sprite.col)
         state.entities
-          .collectFirst{case pos:Position if pos.id == sprite.id => pos}
-          .foreach(pos => g2d.fillRect(pos.x * size, pos.y * size, size, size))
+          .collectFirst { case pos: Position if pos.id == sprite.id => pos }
+          .foreach{pos =>
+            g2d.fillRect((pos.x + offset - playerX) * size, (pos.y + offset - playerY) * size, size, size)}
     }
   }
 }
